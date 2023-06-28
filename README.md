89875d3804edc4cf5a17fa08a2a85c1d7935277d
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-06-28 13:16:14 -0500

    Bubble Sort With Optimization

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# go-algorithm
+# Bubble Sort
+- Bubble Sort without Optimization
+- Bubble Sort with Optimization
+
+# Other problems
+- Sort a list of 25 numbers in reverse order
+- Sort a list of animals in alphabetical order
+- Sort a User object by last, then first name
+
+
+# Resources
+- [Let's Learn Algorithms: Implementing Bubble Sort - Jon Calhoun](https://youtu.be/CAOoBjLATYI)
--- bubble-sort/with-optimization/main.go
+++ bubble-sort/with-optimization/main.go
@@ -13,22 +13,29 @@ func bubbleSort(numbers []int) {
 	var N int = len(numbers)
 	var i int
 	for i = 0; i < N; i++ {
-		sweep(numbers)
+		fmt.Println("Doing a sweep:", numbers)
+		if !sweep(numbers, i) {
+			return
+		}
 	}
 }
-func sweep(numbers []int) {
+
+func sweep(numbers []int, prevPasses int) bool {
 	var N int = len(numbers)
 	var firstIndex int = 0
 	var secondIndex int = 1
+	var didSwap bool = false
 
-	for secondIndex < N {
+	for secondIndex < (N - prevPasses) {
 		var firstNumber int = numbers[firstIndex]
 		var secondNumber int = numbers[secondIndex]
 		if firstNumber > secondNumber {
 			numbers[firstIndex] = secondNumber
 			numbers[secondIndex] = firstNumber
+			didSwap = true
 		}
 		firstIndex++
 		secondIndex++
 	}
+	return didSwap
 }
